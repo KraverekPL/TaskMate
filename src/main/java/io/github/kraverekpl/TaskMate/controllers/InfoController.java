ff8622e5c77fded357408fbcdca35f6aed839d5e
Remigiusz Miller <remigiuszmiller@gmail.com>
2024-08-29 22:56:01 +0200

    adding basic flywave implementation

--- src/main/java/io/github/kraverekpl/TaskMate/controllers/InfoController.java
+++ src/main/java/io/github/kraverekpl/TaskMate/controllers/InfoController.java
@@ -1,27 +1,27 @@
 package io.github.kraverekpl.TaskMate.controllers;
 
 import io.github.kraverekpl.TaskMate.TaskConfigurationProperties;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "/info")
 public class InfoController {
 
     private DataSourceProperties dataSourceProperties;
     private TaskConfigurationProperties taskConfigurationProperties;
-    public InfoController(DataSourceProperties dataSourceProperties,TaskConfigurationProperties taskConfigurationProperties) {
+
+    public InfoController(DataSourceProperties dataSourceProperties, TaskConfigurationProperties taskConfigurationProperties) {
         this.dataSourceProperties = dataSourceProperties;
         this.taskConfigurationProperties = taskConfigurationProperties;
     }
-    @GetMapping(path = "/info/url")
+
+    @GetMapping(path = "/url")
     String url() {
         return dataSourceProperties.getUrl();
     }
 
-    @GetMapping(path = "/info/allowMultipleTasksFromTemplate")
+    @GetMapping(path = "/allowMultipleTasksFromTemplate")
     boolean allowMultipleTasksFromTemplate() {
         return taskConfigurationProperties.getTemplate().isAllowMultipleTasks();
     }
