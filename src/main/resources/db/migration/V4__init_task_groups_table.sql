c12c5f337dbd4620075a9e8036299ee23c05e8bf
Remigiusz Miller <remigiuszmiller@gmail.com>
2024-09-02 22:07:49 +0200

    mofify V4 migration table

--- src/main/resources/db/migration/V4__init_task_groups_table.sql
+++ src/main/resources/db/migration/V4__init_task_groups_table.sql
@@ -1,9 +1,9 @@
-drop table if exists task_groups;
-create table task_groups
+drop table if exists task_group;
+create table task_group
 (
     id          int primary key auto_increment,
     description varchar(255) not null,
     done        bit
 );
 alter table task add column task_group_id int null;
-alter table task add foreign key (task_group_id) references task_groups(id);
+alter table task add foreign key (task_group_id) references task_group(id);
