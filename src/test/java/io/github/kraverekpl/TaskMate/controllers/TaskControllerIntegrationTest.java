13becc892d9a4fcde88888b8a839d74f1f318a2b
Remigiusz Miller <remigiuszmiller@gmail.com>
2024-09-09 10:50:20 +0200

    added new integration test for controler getTask

---
+++ src/test/java/io/github/kraverekpl/TaskMate/controllers/TaskControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package io.github.kraverekpl.TaskMate.controllers;
+
+import io.github.kraverekpl.TaskMate.models.Task;
+import io.github.kraverekpl.TaskMate.models.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class TaskControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    void httpGetReturnGivenTask() throws Exception {
+        //given
+        int taskId = taskRepository.save(new Task("Task 1", LocalDateTime.now(), "Task name A")).getId();
+
+        //when+then
+        mockMvc.perform(get("/tasks/" + taskId))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk());
+    }
+
+}
